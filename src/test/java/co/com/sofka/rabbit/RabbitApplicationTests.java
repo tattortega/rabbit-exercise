91523fe4eb750b51eec16a05f1055068f3190d25
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-13 16:08:43 -0500

    Ejercicio usando rabbit

---
+++ src/test/java/co/com/sofka/rabbit/RabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
